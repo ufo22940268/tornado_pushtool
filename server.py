c747ecc48994a82ecdcf785005452db3e8a52b25
ufo22940268 <ufo22940268@gmail.com>
2012-08-24 18:02:40 +0800

    asdfadsf

--- server.py
+++ server.py
@@ -11,8 +11,13 @@ class MainHandler(tornado.web.RequestHandler):
         loader = template.Loader("/home/ccheng/workspace_tb/tornado_pushtool")
         self.write(loader.load("index.html").generate())
 
+class LoadingHandler(tornado.web.RequestHandler):
+    def get(self):
+        sys.stderr.write("argument:" + self.get_argument("ip") + "\n")
+
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/loading", LoadingHandler),
 ])
 
 if __name__ == "__main__":
